6e3025ff63ecce2dd46e8a0a26aa4a956ff907ef
John Sanchez <jsanche4@uncc.edu>
2016-12-11 05:07:22 +0000

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,6 @@
 # Default is "World"
 # Author: John Sanchez (jSanchez@corp.com)
-name = ARGV.first || "World"
+puts "What's your name"
+my_name = gets.strip
 
-puts "Hello, #{name}!"
+puts "Hello, #{my_name}!"
